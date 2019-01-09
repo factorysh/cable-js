da7d879ed23c22b1843f7856ee9772ac0f09f801
Mathieu Lecarme <mlecarme@bearstech.com>
2019-01-09 22:35:48 +0100

    extract stuff from SVG

---
+++ prepare.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from lxml import etree
+
+
+def layers(dessin: etree.Element) -> (str, etree.Element):
+    for layer in dessin.findall('{http://www.w3.org/2000/svg}g'):
+        label = layer.get('{http://www.inkscape.org/namespaces/inkscape}label')
+        yield label, layer
+
+
+def main(path='dessin.svg'):
+    dessin = etree.fromstring(open(path, 'rb').read())
+    for label, layer in layers(dessin):
+        # print(label, etree.tostring(layer))
+        layer.attrib['id'] = label
+        visit(layer, clean)
+        print(etree.tostring(layer))
+
+
+def clean(element):
+    for k in element.keys():
+        if k.startswith('{'):
+            del element.attrib[k]
+
+
+def tag_name(element):
+    print(element.tag)
+    print("\t", element.attrib, "\n")
+
+
+def visit(node, on_element):
+    on_element(node)
+    for child in node:
+        visit(child, on_element)
+
+if __name__ == '__main__':
+    main()
