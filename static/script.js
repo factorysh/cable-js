5a21ab725ff237588cfd39c21c3d3534bd22307d
Mathieu Lecarme <mlecarme@bearstech.com>
2019-01-16 22:53:06 +0100

    More boxes

--- static/script.js
+++ static/script.js
@@ -116,6 +116,8 @@ cables.newBox("php", "private")
 cables.cable("web", "php")
 cables.newBox("db", "private")
 cables.cable("php", "db")
+cables.newBox("memcache", "private")
+cables.cable("php", "memcache")
 
 /*
 function dragStart(e) {
