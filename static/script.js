e948bc637bb40c40a6cd3122c36be35558bf1d2b
Mathieu Lecarme <mlecarme@bearstech.com>
2019-01-19 16:27:42 +0100

    More object, more const

--- static/script.js
+++ static/script.js
@@ -1,13 +1,16 @@
 import { Cables } from "./lib/cable.js"
 
+const cables = new Cables("#zone svg")
 
-let cables = new Cables("#zone svg")
-cables.newBox("home", "route")
-cables.newBox("web", "public")
-cables.cable("home", "web")
-cables.newBox("php", "private")
-cables.cable("web", "php")
-cables.newBox("db", "private")
-cables.cable("php", "db")
-cables.newBox("memcache", "private")
-cables.cable("php", "memcache")
+const home = cables.newBox("home", "route")
+const web = cables.newBox("web", "public")
+cables.cable(home, web)
+
+const php = cables.newBox("php", "private")
+cables.cable(web, php)
+
+const db = cables.newBox("db", "private")
+cables.cable(php, db)
+
+const memcache = cables.newBox("memcache", "private")
+cables.cable(php, memcache)
